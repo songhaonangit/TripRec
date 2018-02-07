97abcb1ed669822e181fb1dbe051eb6e0ce70738
gagovski <gagovski@163.com>
2018-02-07 21:32:14 +0800

    update playback video

--- app/src/main/java/com/gc/triprec/PlaybackVideoActivity.java
+++ app/src/main/java/com/gc/triprec/PlaybackVideoActivity.java
@@ -1,13 +1,58 @@
 package com.gc.triprec;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class PlaybackVideoActivity extends AppCompatActivity {
+public class PlaybackVideoActivity extends AppCompatActivity implements MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener{
+    private VideoView m_videoView;
+    private MediaController m_mediaController;
+    private int m_position = -1;
+    private static final String TAG = "PlaybackVideoActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playback_video);
+        m_mediaController = new MediaController(this);
+        m_videoView = findViewById(R.id.video_view);
+        m_videoView.setMediaController(m_mediaController);
+    }
+
+    @Override
+    protected void onStart() {
+        Uri uri = getIntent().getParcelableExtra("video");
+        m_videoView.setVideoURI(uri);
+        m_videoView.start();
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        m_position = m_videoView.getCurrentPosition();
+        m_videoView.stopPlayback();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (m_position > 0) {
+            m_videoView.seekTo(m_position);
+            m_position = -1;
+        }
+        super.onResume();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        this.finish();
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
     }
 }
---
+++ app/src/main/java/com/gc/triprec/PlaybackVideoView.java
@@ -0,0 +1,35 @@
+package com.gc.triprec;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * Created by jyin on 07/02/2018.
+ */
+
+public class PlaybackVideoView extends VideoView {
+
+    public PlaybackVideoView(Context context) {
+        super(context);
+    }
+
+    public PlaybackVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PlaybackVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public PlaybackVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(0, widthMeasureSpec);
+        int height = getDefaultSize(0, heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+}
