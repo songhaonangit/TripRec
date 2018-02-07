33df188d8d5e83db7137caaf6c7105949d705dba
gagovski <gagovski@163.com>
2018-02-08 06:49:43 +0800

    add photo viewpager

---
+++ app/src/main/java/com/gc/triprec/PhotoViewPager.java
@@ -0,0 +1,31 @@
+package com.gc.triprec;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by jyin on 08/02/2018.
+ */
+
+public class PhotoViewPager extends ViewPager {
+    private static final String TAG = "PhotoViewPager";
+
+    public PhotoViewPager(Context context) {
+        super(context);
+    }
+
+    public PhotoViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
