cd41a441d109ffe6499d2883086e323587161947
ian <ian_yin@gcoreinc.com>
2018-02-07 16:55:55 +0800

    add Constants

---
+++ app/src/main/java/com/gc/triprec/Constants.java
@@ -0,0 +1,14 @@
+package com.gc.triprec;
+
+public class Constants {
+
+    public static class CameraConstans {
+        public final static int CAM_FRONT   = 0;
+        public final static int CAM_REAR    = 1;
+    }
+
+    public static class SettingConstants {
+        public final static String KEY_RECORD_ENABLE    = "recorden";
+        public final static String KEY_RECORD_TIME      = "recordtime";
+    }
+}
